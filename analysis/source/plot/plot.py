d5910c7b373253c9daf6064d10364b8771e3bc12
Houda Nait <naitelbarjhouda@gmail.com>
2020-07-22 09:49:38 -0700

    Update the plot

--- analysis/source/plot/plot.py
+++ analysis/source/plot/plot.py
@@ -1,10 +1,15 @@
 import yaml
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     CONFIG =  yaml.load(open('config_global.yaml', 'rU'))
-
-    plt.hist('%s/draws.csv' % CONFIG['build']['draw_data'])
+    data = np.genfromtxt('%s/draws.csv' % CONFIG['build']['draw_data'], delimiter =",")
+    plt.hist(data)
+    plt.xlabel('Draw')
+    plt.ylabel('Count')
+    plt.title('Histogram')
+    plt.text(2, 1700, r'$N(0,1)$')
     plt.savefig('%s/draw_plot.eps' % CONFIG['build']['plot'])
 
 main()
